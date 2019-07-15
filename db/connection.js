c34a5b640fd3bfab85e94319f82f59d8bc62826e
Hammad Malik <tomatohammado@gmail.com>
2019-07-15 12:22:08 -0400

    Initialize db/connection.js

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = Promise;
+
+const mongoURI = "mongodb://localhost/monarch-api";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(err => console.log("Connection Failed.", err));
+
+module.exports = mongoose;
